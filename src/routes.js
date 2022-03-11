e7217be5f92dcf304f2c904fc1d763623182ef1a
birdhins <bintangardhiansyah73@gmail.com>
2022-03-11 13:19:18 +0700

    initial comit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const { handler } = require('@hapi/hapi/lib/cors');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNotesByIdHandler } = require('./handler');
+
+const routes = [
+    {
+      method: 'POST',
+      path: '/notes',
+      handler: addNoteHandler,
+    },
+    {
+      method: 'GET',
+      path: '/notes',
+      handler: getAllNotesHandler,
+    },
+    {
+      method: 'GET',
+      path:'/notes/{id}',
+      handler: getNoteByIdHandler,
+    },
+    {
+      method:'PUT',
+      path: '/notes/{id}',
+      handler: editNoteByIdHandler,
+    },
+    {
+      method:'DELETE',
+      path: '/notes/{id}',
+      handler: deleteNotesByIdHandler,
+    }
+  ];
+   
+  module.exports = routes;
